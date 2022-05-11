25b87b885ce6c26aa86ddf477e12236b1a69fd65
Felipe Vallejo <stephanovallejom@duck.com>
2022-05-11 13:30:56 -0500

    Search logic implemented with test-driven developement

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,8 +8,6 @@ fn main(){
         println!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
