33e005450db654c95fd3a2e27ccba2da0ffd3be7
Felipe Vallejo <stephanovallejom@duck.com>
2022-06-01 14:42:34 -0500

    Adding iterators

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,10 +3,9 @@ use std::env;
 use minigrep::Config;
 
 fn main(){
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
-        std::process::exit(1);
+        process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
