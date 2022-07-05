6ff53acf6d337daab251aba758589c878a1d7e59
Felipe Vallejo <stephanovallejom@duck.com>
2022-07-05 14:57:49 -0500

    Primitive types and structs

---
+++ rustlings/primitive_types3.rs
@@ -0,0 +1,17 @@
+// primitive_types3.rs
+// Create an array with at least 100 elements in it where the ??? is.
+// Execute `rustlings hint primitive_types3` for hints!
+
+fn main() {
+    // pay attention we need to use a colon
+    let a = [8; 100];
+
+    println!("{}", a[2]);
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
+
