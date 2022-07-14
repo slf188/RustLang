6c289193411c52dc67ce52344110542c4c79d73a
Felipe Vallejo <stephanovallejom@duck.com>
2022-07-14 15:21:48 -0500

    Multithreaded web server finished

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,3 +1,6 @@
+use server::ThreadPool;
+use std::thread;
+use std::time::Duration;
 use std::fs;
 // to read and write from the stream
 use std::io::prelude::*;
@@ -11,29 +14,36 @@ fn main() {
     // bind means binding to a port
     // bind returns Result<T, E> i.e. the binding might fail
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     // incoming is an iterator that returns a sequence of streams
     // each stream resembles an open connection between client and server
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         // unwrap will terminate our program if the stream has any errors
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down");
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    // buffer will hold the data that is read
     let mut buffer = [0; 1024];
-    // stream.data will read bytes and put those in the buffer
     stream.read(&mut buffer).unwrap();
+
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
-
     let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!(
