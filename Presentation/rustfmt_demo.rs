5e174e893c5bcf90c8605d5631705277bf68bd9b
菲利 <stephanovallejom@duck.com>
2022-11-10 09:20:37 -0500

    Create rustfmt_demo.rs

---
+++ Presentation/rustfmt_demo.rs
@@ -0,0 +1,14 @@
+fn print_out(say: String)
+
+{
+  println!("The users message is {}", say);
+      println!("This program will be used further on to demonstrate how rustfmt can save us a lot of time formatting our programs");
+            println!("Demo is coming up");
+println!("Hey");
+    println!("Ok this is crazy");
+
+}
+
+fn main(){
+  let say = String::from("Rust is the best language out there");
+    print_out(say);}
