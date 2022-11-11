afb5e6a45ea95bab25a0d95c3e73491537f34d7a
菲利 <stephanovallejom@duck.com>
2022-11-11 09:57:33 -0500

    Update ownership_reference.rs

--- Presentation/ownership_reference.rs
+++ Presentation/ownership_reference.rs
@@ -1,11 +1,27 @@
 fn main(){
+  /* example 1
+  stack: x | i32 | 5
+  */
+  let x: i32 = 5;
+  
+  /* example 2
+  stack: vec1 | pointer -> heap: 1, 2, 3, 4
+  */
+  let mut vec1 = vec![1, 2, 3];
+  vec1.push(4);
+  println!("{}", vec1);
+  drop(vec1);
+  
+  // example 3
   let mut say = String::from("Do");
   say.push_str("g");
-  print_out(&say);
-  println!("The original string var's value is {}", say);
   let say2 = say;
   drop(say);
-  println!("The copy would therefore contain {}", say2);
+
+  // example 4
+  let say3 = String::from("Cat");
+  print_out(&say3);
+  println!("The original string var's value is {}", say3);
 }
 
 fn print_out(say: &String){
