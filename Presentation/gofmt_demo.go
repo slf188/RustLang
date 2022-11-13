5e88431e0029621557c0efb0432e581bb8d607e6
菲利 <stephanovallejom@duck.com>
2022-11-12 19:27:45 -0500

    Create gofmt_demo.go

---
+++ Presentation/gofmt_demo.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() 
+
+{
+			fmt.Println("Hello World")
+greeting := "Hello I'm Golang, I also have a formatter like Rust"
+fmt.Println(greeting)
+	// write a for loop that prints out go 3 times
+	for i := 0; i < 3; i++ {
+		fmt.Printnl("Go")
+	}
+		}
