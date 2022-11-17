7865cfd801b2bfd4a35a9b0affd45e7e7f2e51a5
菲利 <stephanovallejom@duck.com>
2022-11-17 09:38:58 -0500

    Create singlyLinkedList.rs

---
+++ Presentation/singlyLinkedList.rs
@@ -0,0 +1,48 @@
+struct Node {
+    data: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    // constructor
+    fn new(data: i32) -> Node {
+        Node {
+            data: data,
+            next: None,
+        }
+    }
+    
+    // dos argumentos: referencia al nodo y un vector i32
+    fn create(node: &mut Node, data: Vec<i32>) {
+        // nuevo nodo
+        let new_node = Node::new(data[0]);
+        // asignar el siguiente nodo al nuevo nodo
+        node.next = Some(Box::new(new_node));
+        // si la longitud del vector es mayor que 1
+        if data.len() > 1 {
+            // crear un nuevo vector con los datos del anterior vector
+            let mut new_data = Vec::new();
+            for i in 1..data.len() {
+                new_data.push(data[i]);
+            }
+            // llamar la función create de nuevo
+            Node::create(&mut node.next.as_mut().unwrap(), new_data);
+        }
+    }
+
+    // imprimir la lista
+    fn display(node: &Node) {
+        print!("{} ", node.data);
+        if node.next.is_some() {
+            Node::display(&node.next.as_ref().unwrap());
+        }
+    }
+}
+
+fn main(){
+    let mut node = Node::new(1);
+    let data = vec![2, 3, 4, 5, 6, 7, 8, 9, 10];
+    Node::create(&mut node, data);
+    Node::display(&node);
+    println!();
+}
